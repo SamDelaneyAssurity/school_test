fd3e3f7c237902082606bb547882a721bff4f9c2
Sam Delaney <sam.delaney@assurity.co.nz>
2021-07-16 11:00:54 +1200

    Testing student

---
+++ src/test/java/steps/steps.java
@@ -0,0 +1,34 @@
+package steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class steps {
+    private Response response;
+    @When("I check the details of student {int}")
+    public void iCheckTheDetailsOfStudent(int arg0) {
+        RestAssured.baseURI = "https://it-foundations.app.ap.assurity.cloud/";
+        response = RestAssured.get("people/" + arg0);
+
+
+    }
+
+    @Then("I can see that their name is {string}")
+    public void iCanSeeThatTheirNameIsJaneJones(String arg0) {
+
+        String temp = response.path("firstName") +" "+response.path("lastName");
+
+        assertEquals(temp,arg0 );
+    }
+
+    @And("they have a {string} from {string}")
+    public void theyHaveABScComputerScienceFromSydneyTechSchool(String arg0, String arg2) {
+        assertEquals(response.path("degree"),arg0 );
+        assertEquals(response.path("university"),arg2 );
+    }
+}
